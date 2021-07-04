ae06cb1654da59ab774df507a67c409a74ab6fb8
Aliaksei Kisel <aliaksei_kisel@epam.com>
2021-07-04 18:43:37 +0300

    timer

--- app/src/main/java/com/example/stopwatch/StopwatchViewHolder.kt
+++ app/src/main/java/com/example/stopwatch/StopwatchViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.stopwatch
 
+import android.os.CountDownTimer
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stopwatch.databinding.StopwatchItemBinding
 
@@ -7,8 +8,40 @@ class StopwatchViewHolder(
     private val binding: StopwatchItemBinding
 ): RecyclerView.ViewHolder(binding.root) {
 
+    private val timer: CountDownTimer? = null
+
     fun bind(stopwatch: Stopwatch) {
         binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+
+        if (stopwatch.isStarted) {
+            startTimer(stopwatch)
+        }
+    }
+
+    private fun startTimer(stopwatch: Stopwatch) {
+
+    }
+
+    private fun getCountDownTimer(stopwatch: Stopwatch): CountDownTimer {
+        return object : CountDownTimer(
+            timer.periodMillis - timer.currentMillis,
+            UNIT_TEN_MS
+        ) {
+            val interval = UNIT_TEN_MS
+
+            override fun onTick(millisUntilFinished: Long) {
+                stopwatch.currentMs += interval
+
+            }
+
+            override fun onFinish() {
+                binding.timerView.setCurrent(timer.currentMillis)
+                timer.currentMillis = 0
+                listener.stopTimer(timer)
+                val color = binding.root.context.resources.getColor(R.color.deep_orange_100_dark)
+                binding.root.setCardBackgroundColor(color)
+            }
+        }
     }
 
     private fun Long.displayTime(): String {
@@ -34,5 +67,6 @@ class StopwatchViewHolder(
     private companion object {
 
         private const val START_TIME = "00:00:00:00"
+        private const val UNIT_TEN_MS = 10L
     }
 }
